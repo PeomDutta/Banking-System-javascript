67f8b68594f85fa27297f66133f4146e29cf44c3
itspeomd <peomd04@gmail.com>
2021-08-21 02:37:29 +0600

    withdraw-update

--- js/banking.js
+++ js/banking.js
@@ -1,8 +1,8 @@
 //--------- deposite button handle ----------//
 document.getElementById("deposit-button").addEventListener('click', function () {
     const depositInputField = document.getElementById("deposit-input");
-    depositInputFieldText = depositInputField.value;
-    depositInputFieldValue = parseFloat(depositInputFieldText);
+    const depositInputFieldText = depositInputField.value;
+    const depositInputFieldValue = parseFloat(depositInputFieldText);
 
     console.log(depositInputFieldText);
 
@@ -13,6 +13,55 @@ document.getElementById("deposit-button").addEventListener('click', function () 
 
     depositeShow.innerText = totalDepositAmount;
 
+
+    // balance after deposit handle 
+    const balanceShow = document.getElementById("balance-show");
+    const balanceShowTotal = balanceShow.innerText;
+    const balanceShowTotalValue = parseFloat(balanceShowTotal);
+
+
+    const totalBalanceAmount = balanceShowTotalValue + depositInputFieldValue;
+
+
+    balanceShow.innerText = totalBalanceAmount;
+
+    // clear deposit money
     depositInputField.value = "";
+
+})
+
+
+
+
+//------------------ withdraw button handle ---------------------//
+document.getElementById("withdraw-button").addEventListener('click', function () {
+    const withdrawInputField = document.getElementById("withdraw-input");
+    const withdrawInputFieldText = withdrawInputField.value;
+    const withdrawInputFieldValue = parseFloat(withdrawInputFieldText);
+
+    console.log(withdrawInputFieldValue);
+
+    const withdrawShow = document.getElementById("withdraw-show");
+    const withdrawShowTotal = withdrawShow.innerText;
+    const withdrawShowTotalValue = parseFloat(withdrawShowTotal);
+    const totalWithdrawAmount = withdrawShowTotalValue + withdrawInputFieldValue;
+
+    withdrawShow.innerText = totalWithdrawAmount;
+
+
+    // balance after withdraw handle 
+
+    const balanceShow = document.getElementById("balance-show");
+    const balanceShowTotal = balanceShow.innerText;
+    const balanceShowTotalValue = parseFloat(balanceShowTotal);
+
+
+    const totalBalanceAmount = balanceShowTotalValue - withdrawInputFieldValue;
+
+
+    balanceShow.innerText = totalBalanceAmount;
+
+    // clear withdraw money
+    withdrawInputField.value = "";
 
 })
